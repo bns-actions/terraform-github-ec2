00edee55cc9ccda613a3c343b3e8891cff3a1414
unknown <user.name>
2026-08-08 15:44:58 +0530

    terraform-github

--- provider.tf
+++ provider.tf
@@ -5,12 +5,7 @@ terraform {
       version = "5.84.0"
     }
   }
-  backend "s3" {
-    bucket = "bnsaws-remote-state"
-    key    = "github-action"
-    region = "us-east-1"
-     use_lockfile = true
-  }
+  
 }
 
 
