64b0c72620a2aa7043d5dd86703371dacb0405f2
unknown <user.name>
2026-08-08 15:17:56 +0530

    terraform-github

--- provider.tf
+++ provider.tf
@@ -5,7 +5,14 @@ terraform {
       version = "5.84.0"
     }
   }
+  backend "s3" {
+    bucket = "bnsaws-remote-state"
+    key    = "github-action"
+    region = "us-east-1"
+    dynamodb_table = "bnsaws-locking"
+  }
 }
+
 
 provider "aws" {
   # Configuration options
